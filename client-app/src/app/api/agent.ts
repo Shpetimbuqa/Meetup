4cc71f762d4daeb7117acdac5616988ae4e5a8a3
Shpetim <shpetim.buqa@hotmail.com>
2021-07-01 12:00:05 +0200

    Submit

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -28,16 +28,16 @@ const request = {
     del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 }
 
-const activities = {
+const Activities = {
     list: () => request.get<Activity[]>('/activities'),
-    details: (id: string) => request.get<Activity>('/activities/${id}'),
+    details: (id: string) => request.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>('/activities/$(activity.id)', activity),
-    delete: (id: string) => axios.delete<void>('activities/${id}'),
+    update: (activity: Activity) => axios.put<void>(`/activities/$(activity.id)`, activity),
+    delete: (id: string) => axios.delete<void>(`activities/${id}`)
 }
 
 const agent = {
-    activities
+    Activities
 }
 
 export default agent;
